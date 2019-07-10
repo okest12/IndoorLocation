c43870db279b5296a43b5bd975c422405f04050f
okest <49010388+okest12@users.noreply.github.com>
2019-07-10 12:46:51 +0800

    Add files via upload

--- Kathy/training.py
+++ Kathy/training.py
@@ -1,4 +1,5 @@
 import numpy as np
+import xlwt
 np.set_printoptions(threshold=np.inf)
 
 g_cellMap=[]
@@ -20,7 +21,7 @@ def getmaxmap():
     global maxX
     global maxY
     isfirstline = 1
-    with open(r'C:\03-study\MachineLearning\train.csv', 'r') as f:
+    with open('train.csv', 'r') as f:
         for line in f.readlines():
             if isfirstline:
                 isfirstline=0
@@ -33,8 +34,7 @@ def getmaxmap():
                 if y>maxY:
                    maxY= y
         #print(maxX," ",maxY)
-                
-#def buildinitmaps():
+
 
 
 def buildmap():
@@ -42,7 +42,7 @@ def buildmap():
     global eNbcount
     global g_cellMap
     getmaxmap()
-    with open(r'C:\03-study\MachineLearning\train.csv', 'r') as f:
+    with open('train.csv', 'r') as f:
         for line in f.readlines():
             if linenum == 0:
                 firstline = line.strip()
@@ -61,35 +61,72 @@ def buildmap():
             linenum +=1
         #print(g_cellMap[0][0])
 
+
+
 def buildtestmap():
     global g_cellMap
     global eNbcount
+    global maxX
+    global maxY
+    defaultpower = 9999.0
+    numtoselect = 16
+    numtoselect2 = 32
     buildmap()
     
-    with open(r'C:\03-study\MachineLearning\test.csv', 'r') as f:
+    with open('test.csv', 'r') as f:
         linenum = 0
-
-        f1=open(r"C:\03-study\MachineLearning\report.csv","a")
-
-
+        
+        file = xlwt.Workbook()
+        defaule_style = xlwt.XFStyle()
+        nearest_style = xlwt.easyxf('pattern: pattern solid, fore_colour blue')
+        nearest_style2 = xlwt.easyxf('pattern: pattern solid, fore_colour red')
+        new_style = xlwt.easyxf('pattern: pattern solid, fore_colour ice_blue')
         for line in f.readlines():
             if linenum != 0:
                 tmpline = line.strip()
                 posdata = tmpline.split(",")
                 tmp= np.zeros([maxX+1,maxY+1])
+                power_total = 0
+                sheet_name = 'sheet name'+str(linenum)
+                table = file.add_sheet(sheet_name)
+                
                 for i in range(0,eNbcount):
+
+                    power_total += abs(float(posdata[i+2]))
                     tmp = tmp+np.abs(g_cellMap[i]-np.ones([maxX+1,maxY+1])*float(posdata[i+2]))
-                    if (i==eNbcount-1):
-                        minrow = tmp.min()
+                    if i == eNbcount-1:
                         m = np.array(tmp)
-                        print(np.where(m == minrow))
-                        print("min: %s",minrow)
-                        
+                        min_powers = np.argpartition(m.ravel(),numtoselect)[:numtoselect]
+                        min_powers2 = np.argpartition(m.ravel(),numtoselect2)[:numtoselect2]
+
+                        #if linenum == 1:#this line is for shorter test time, to be delete
+                        for k in range(0,maxX):
+                            for n in range(0,maxY):
+                                if m[k][n] == power_total:
+                                    m[k][n]= defaultpower
+                                    table.write(k,n,str(m[k][n]),new_style)
+                                elif m[k][n] <= m.ravel()[min_powers[numtoselect-1]]:
+                                    table.write(k,n,str(m[k][n]),nearest_style)
+                                elif m[k][n] <= m.ravel()[min_powers2[numtoselect2-1]]:
+                                    table.write(k,n,str(m[k][n]),nearest_style2)
+                                else:
+                                    table.write(k,n,str(m[k][n]),defaule_style)
+
+                        '''
+                        for j in range(0,numtoselect):
+                            print(np.unravel_index(min_powers[j], m.shape))
+                            print(m.ravel()[min_powers[j]])
+                        print("\n")
                         f1.write("linenum"+str(linenum)+"\n")
-                        f1.write(str(tmp)+"\n")
+                        for k in range(0,maxX):
+                            for n in range(0,maxY):
+                                if m[k][n] == power_total:
+                                    m[k][n]= defaultpower
+                                f1.write(str(m[k][n])+",")
+                            f1.write("\n")
+                        f1.write("\n")
+                        '''
             linenum+=1
-    
-            
+        file.save("reportsheets.xls")
     return
          
-buildtestmap()
